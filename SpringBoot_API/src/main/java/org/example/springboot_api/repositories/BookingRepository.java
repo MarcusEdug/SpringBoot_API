afb0fc1591a605eef766952796485c85aaefe8f6
msved <marcus.svedberg.java23@edu.edugrade.se>
2024-04-15 13:06:50 +0200

    Added methods to controller, service and repository. Modified booking entity

--- SpringBoot_API/src/main/java/org/example/springboot_api/repositories/BookingRepository.java
+++ SpringBoot_API/src/main/java/org/example/springboot_api/repositories/BookingRepository.java
@@ -1,7 +1,11 @@
 package org.example.springboot_api.repositories;
 
+import org.example.springboot_api.entities.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public class BookingRepository {
+public interface BookingRepository extends JpaRepository<Booking, Integer> {
 }
