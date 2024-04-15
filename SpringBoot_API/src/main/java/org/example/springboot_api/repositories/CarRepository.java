1e6353bf8458513f21885da80eaa5e87e9b73342
Rosen <Alexander.rosen.JAVA23@edu.edugrade.se>
2024-04-15 11:47:26 +0200

    Crud car

--- SpringBoot_API/src/main/java/org/example/springboot_api/repositories/CarRepository.java
+++ SpringBoot_API/src/main/java/org/example/springboot_api/repositories/CarRepository.java
@@ -1,4 +1,9 @@
 package org.example.springboot_api.repositories;
 
-public class CarRepository {
+import org.example.springboot_api.entities.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepository extends JpaRepository<Car, Integer> {
 }
