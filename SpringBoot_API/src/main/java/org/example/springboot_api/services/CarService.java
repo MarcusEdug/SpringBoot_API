3874acd0a375b7bb41fd49c2eb8fd2121e09db55
msved <marcus.svedberg.java23@edu.edugrade.se>
2024-04-22 11:16:01 +0200

    Added logger to the code

--- SpringBoot_API/src/main/java/org/example/springboot_api/services/CarService.java
+++ SpringBoot_API/src/main/java/org/example/springboot_api/services/CarService.java
@@ -1,6 +1,7 @@
 package org.example.springboot_api.services;
 
 
+import org.apache.log4j.Logger;
 import org.example.springboot_api.entities.Car;
 import org.example.springboot_api.exceptions.ResourceNotFoundException;
 import org.example.springboot_api.repositories.CarRepository;
@@ -14,12 +15,16 @@ import java.util.List;
 @Service
 public class CarService implements CarServiceInterface {
 
+    private static final Logger logger = Logger.getLogger(CarService.class);
     @Autowired
     private CarRepository carRepository;
 
     @Override
     public Car addCar(Car car) {
-        return carRepository.save(car);
+        carRepository.save(car);
+        logger.info("Admin added new car" + " " + car.getRegistrationNumber());
+        return car;
+
     }
 
     @Override
@@ -60,14 +65,16 @@ public class CarService implements CarServiceInterface {
         existingCar.setBookedStatus(car.isBookedStatus());
 
 
-
         carRepository.save(existingCar);
+        logger.info("Admin updated car with registration number " + existingCar.getRegistrationNumber() + " successfully");
         return existingCar;
     }
 
     @Override
     public void deleteCar(int id) {
-        carRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Car", "id", id));
+        Car car = carRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Car", "id", id));
         carRepository.deleteById(id);
+        logger.info("Admin deleted car with registration number " + car.getRegistrationNumber() + " successfully");
+
     }
 }
